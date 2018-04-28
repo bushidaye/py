ce2b31af34a43231eebfc23ff46183cdf7ccbe1c
meng liu <1737119884@qq.com>
2018-04-28 10:39:48 +0800

    Initial commit

---
+++ excel.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+'''
+根据部署文档，结合ansible部署应用
+'''
+import os
+import xlrd
+os.chdir('/home/')
+
+
+def read_excel():
+    role_list = ['zookeeper', 'redis', 'pika', 'kafka', 'cassandra']
+    zookeeper_play = r'ansible-playbook site.yaml -e "role=%s pl=%s"'
+    redis_play = r'ansible-playbook site.yaml -e "role=%s pl=%s"'
+
+    excelfile = xlrd.open_workbook(r'test.xlsx')
+    sheet =  excelfile.sheet_by_name('python')
+    row = [sheet.row_values(i) for i in range(sheet.nrows)]
+
+    for i in range(len(row)):
+         if row[i][0] in role_list:
+            if row[i][0] == 'zookeeper':
+                os.system(zookeeper_play % tuple([row[i][j] for j in range(len(row[i]))]))
+            elif row[i][0] == 'redis':
+                os.system(redis_play % tuple([row[i][j] for j in range(len(row[i]))]))
+
+if __name__ == '__main__':
+    read_excel()
