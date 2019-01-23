edd33dc7c8b115bf3d76eab0c3e26847b3717aff
bushidaye <1737119884@qq.com>
2019-01-23 17:12:00 +0800

    add file-comapr1

---
+++ scritp/file-compare1.py
@@ -0,0 +1,15 @@
+# _*_coding:utf-8_*_
+'''
+选出两个文件的交集
+192.168.0.1 项目 --> 192.168.0.1
+'''
+with open('im') as im,open('zx') as zx:
+    fim=im.readlines()
+    fzx=zx.readlines()
+    im.close()
+    zx.close()
+    
+    for i in fim:
+        for j in fzx:
+            if i.find(j.split()[0])==0:
+                print i
